d4c00248f85aa1b8c8e5774099979f4dae53238e
Milo Rastgoo <msrastgoo@gmail.com>
2020-05-21 19:09:31 -0400

    single number refactored, refactoring eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,22 +2,21 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n,b=None):
+def eating_cookies(n,cache=None):
     # Your code here
 
     if n == 0:
         return 1
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    if n == 3:
-        return 4
-    if n == 4:
-        return 7
-    # n == 5 => 8
-    if n > 4:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if n < 0:
+        return 0
+    if cache and cache[n] > 0:
+        return cache[n]
+    else:
+        cache = {}
+        cache[n] = (eating_cookies(n-1,cache) + eating_cookies(n-2,cache) + eating_cookies(n-3,cache))
+        return cache[n]
+    # if n >= 0:
+    #     return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
